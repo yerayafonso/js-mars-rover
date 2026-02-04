9a869d6c21e2aeec1c871fd1066f805c46d2fb4a
yerayafonso <yafonso02@gmail.com>
2026-02-04 02:48:44 +0000

    add functionality to create visual of the plateau with start and end positions of rover

--- main.js
+++ main.js
@@ -5,17 +5,40 @@ const PlateauSize = require("./plateauSize");
 const InputParser = require("./inputLayer/inputParser");
 const Rover = require("./logicLayer/roverClass");
 const UserInterface = require("./ui/uiLayer.js");
+const { getPlateau } = require("./plateauStore.js");
 
 const user = new UserInterface();
 
 async function run() {
   await user.promptForPlateauSize();
 
+  let plateauGrid = user.printGrid(getPlateau());
+
+  user.formatGrid(plateauGrid);
+
   const rover = await user.promptForRoverLandingCoordinates();
+
+  user.placeRoverOnGrid(plateauGrid, rover);
+
+  user.formatGrid(plateauGrid);
+
+  let plateauGrid2 = user.printGrid(getPlateau());
 
   await user.promptForRoverInstructions(rover);
 
-  await user.promptToEndProgram();
+  user.placeRoverOnGrid(plateauGrid2, rover);
+
+  user.formatGrid(plateauGrid2);
+
+  let plateauGrid3 = user.printGrid(getPlateau());
+
+  await user.promptForRoverInstructions(rover);
+
+  user.placeRoverOnGrid(plateauGrid3, rover);
+
+  user.formatGrid(plateauGrid3);
+
+  // await user.promptToEndProgram();
 }
 
 run();
--- ui/uiLayer.js
+++ ui/uiLayer.js
@@ -8,9 +8,55 @@ const { getPlateau } = require("../plateauStore.js");
 const inquirer = require("inquirer");
 const confirm = require("@inquirer/confirm");
 const run = require("../main.js");
+const chalk = require("chalk");
 
 class UserInterface {
   constructor() {}
+
+  formatGrid(grid) {
+    grid.forEach((row) => {
+      console.log(`[ ${row.join(", ")} ]`);
+    });
+  }
+
+  printGrid(plateau) {
+    let plateauGrid = [];
+    for (let i = 0; i < plateau.maxY; i++) {
+      let gridRow = [];
+      for (let j = 0; j < plateau.maxX; j++) {
+        gridRow.push(0);
+      }
+      plateauGrid.push(gridRow);
+    }
+    return plateauGrid;
+  }
+
+  placeRoverOnGrid(plateauGrid, rover) {
+    for (let i = 0; i < plateauGrid; i++) {
+      for (let j = 0; j < plateauGrid[i]; j++) {
+        plateauGrid[i][j] = 0;
+      }
+    }
+    const plateau = getPlateau();
+    let roverFigure;
+
+    switch (rover.facing) {
+      case "N":
+        roverFigure = "A";
+        break;
+      case "E":
+        roverFigure = ">";
+        break;
+      case "W":
+        roverFigure = "<";
+        break;
+      default:
+        roverFigure = "V";
+    }
+    const coordY = plateau.maxY - rover.posY;
+    plateauGrid[coordY][rover.posX - 1] = roverFigure;
+    return plateauGrid;
+  }
 
   async promptForPlateauSize() {
     const input = await inquirer.prompt([
@@ -29,6 +75,7 @@ class UserInterface {
       console.log("That is not a valid plateau size. Try again!");
       return this.promptForPlateauSize();
     }
+
     console.log(`Plateau Size: (0, 0) - (${plateau.maxX}, ${plateau.maxY})`);
   }
 
@@ -46,6 +93,7 @@ class UserInterface {
       const roverPosition = parser.positionParser(input.roverCoordinates);
       // const plateau = getPlateau();
       const rover = new Rover(roverPosition);
+
       console.log(
         `Rover has landed at (${roverPosition.X}, ${roverPosition.Y}) facing ${roverPosition.facingDirection}`,
       );
@@ -71,15 +119,18 @@ class UserInterface {
       const parser = new InputParser();
       const roverInstructions = parser.instructionParser(input.instructions);
 
-      roverClass.control(roverInstructions);
+      const updatedRover = roverClass.control(roverInstructions);
       console.log(
         `The rover is now at (${roverClass.posX}, ${roverClass.posY}) facing ${roverClass.facing}`,
       );
+
+      return updatedRover;
     } catch (error) {
       console.log("These are invalid instructions. Try again!");
       return this.promptForRoverInstructions(roverClass);
     }
   }
+
   async promptToEndProgram() {
     try {
       const answer = await confirm([
